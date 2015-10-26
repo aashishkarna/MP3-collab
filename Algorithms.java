de1c1378e2aa3a425a7e88b9fbc386801d0df0a2
Aashish Karna <abc@123.com>
2015-10-26 11:05:07 -0700

    completed algorithms

--- Algorithms.java
+++ Algorithms.java
@@ -6,7 +6,6 @@ import ca.ubc.ece.cpen221.mp3.staff.Vertex;
 
 public class Algorithms {
 
-	int distance = 0;
 	/**
 	 * *********************** Algorithms ****************************
 	 * 
@@ -26,122 +25,18 @@ public class Algorithms {
 	 * @return
 	 */
 	public static int shortestDistance(Graph graph, Vertex a, Vertex b) {
-
-		int minDistance1 = Integer.MAX_VALUE;
-		int minDistance2 = Integer.MAX_VALUE;
-		 Set<List<Vertex>> depthFirst = depthFirstSearch(graph,a,b);
-		Set<List<Vertex>> breadthFirst = breadthFirstSearch(graph, a, b);
-		for (List<Vertex> path : breadthFirst) {
-			if (path.size() < minDistance1) {
-				minDistance1 = path.size();
-			}
-		}
-		for (List<Vertex> path : depthFirst) {
-			if (path.size() < minDistance2) {
-				minDistance2 = path.size();
-			}
+		int distance = 0;
+		if (a.equals(b)) {
+			return 0;
+		} else {
+			distance = searcherDFS(graph, a, b);
+			return distance;
 		}
 
-		if(minDistance1<minDistance2){
-			return minDistance1;
-		}else{
-			return minDistance2;
-		}
+	
 	}
 
-	public static Set<List<Vertex>> breadthFirstSearch(Graph g, Vertex a, Vertex b) {
-
-//		List<Vertex> vertexList = new ArrayList<>();
-		Set<List<Vertex>> listSet = new HashSet<>();
-
-
-	//	List<Vertex> allVertices = new ArrayList<>();
-
-
-
-			Queue<Vertex> vertexQueue = new LinkedList<Vertex>();
-			List<Vertex> discoveredVertices = new ArrayList<>();
-			List<Vertex> branch = new ArrayList<>();
-			vertexQueue.add(a);
-			while (!vertexQueue.isEmpty()) {
-				Vertex w = vertexQueue.poll();
-				
-				if(!discoveredVertices.contains(w)) {
-					discoveredVertices.add(w);
-					branch.add(w);
-					for (Vertex z : g.getDownstreamNeighbors(w)) {
-						vertexQueue.add(z);
-					}
-				}
-				
-				
-				if (w.equals(b)){
-					break;
-				}
-			}
-			listSet.add(branch);
-		
-		
-/*		for (Vertex temp : l) {
-			if (!discoveredVertices.contains(temp)) {
-				discoveredVertices.add(temp);
-			}
-			if (temp.equals(b)) {
-				vertexList.add(a);
-				vertexList.add(b);
-
-				listSet.add(vertexList);
-			} else {
-				allDownstream = g.getDownstreamNeighbors(temp);
-				Set<List<Vertex>> search1 = new HashSet<>();
-				search1 = breadthFirstSearch(g, temp, b, allDownstream);
-
-				for (List<Vertex> newPath : search1) {
-					newPath.add(0, a);
-					listSet.add(newPath);
-				}
-
-			}
-
-			// listSet.add(discoveredVertices);
-			// discoveredVertices.clear();
-		}*/
-
-		return listSet;
-
-	}
-
-	public static Set<List<Vertex>> depthFirstSearch(Graph g, Vertex a, Vertex b) {
-
-//		List<Vertex> downStreamVertices = new ArrayList<>();
-		List<Vertex> discoveredVertices = new ArrayList<>();
-		Set<List<Vertex>> listSet = new HashSet<>();
-		Stack<Vertex> vertexStack = new Stack<>();
-		
-//		downStreamVertices = g.getDownstreamNeighbors(a);
-
-		List<Vertex> branch = new ArrayList<>();
-		vertexStack.push(a);
-		while (!vertexStack.empty()) {
-			Vertex w = vertexStack.pop();
-			
-			
-			if (!discoveredVertices.contains(w)) {
-				discoveredVertices.add(w);
-				branch.add(w);
-				for (Vertex z : g.getDownstreamNeighbors(w)) {
-					vertexStack.add(z);
-				}
-			}
-			if(w.equals(b)){
-				break;
-			}
-		}
-		listSet.add(branch);
-
-		return listSet;
-
-	}
+	
 
 	public static List<Vertex> commonUpstreamVertices(Graph g, Vertex a, Vertex b) {
 
@@ -184,15 +79,15 @@ public class Algorithms {
 			vertexStack.push(v);
 			while (!vertexStack.empty()) {
 				Vertex w = vertexStack.pop();
-				
+				branch.add(w);
 				if (!discoveredVertices.contains(w)) {
 					discoveredVertices.add(w);
-					branch.add(w);
+
 					for (Vertex z : g.getDownstreamNeighbors(w)) {
 						vertexStack.add(z);
 					}
 				}
-				
+
 			}
 			listSet.add(branch);
 		}
@@ -213,20 +108,72 @@ public class Algorithms {
 			vertexQueue.add(v);
 			while (!vertexQueue.isEmpty()) {
 				Vertex w = vertexQueue.poll();
-				
-				if(!discoveredVertices.contains(w)) {
+				branch.add(w);
+				if (!discoveredVertices.contains(w)) {
 					discoveredVertices.add(w);
-					branch.add(w);
+
 					for (Vertex z : g.getDownstreamNeighbors(w)) {
 						vertexQueue.add(z);
 					}
 				}
-				
+
 			}
 			listSet.add(branch);
 		}
 
 		return listSet;
 	}
-	
+
+	public static int searcherDFS(Graph g, Vertex a, Vertex b) {
+//		Set<List<Vertex>> listSet = new HashSet<>();
+//		List<Vertex> allVertices = new ArrayList<>();
+		int counter = 0;
+
+//		allVertices = g.getVertices();
+
+		Queue<Vertex> vertexQueue = new LinkedList<Vertex>();
+		Queue<Vertex> downstreamQueue = new LinkedList<Vertex>();
+		List<Vertex> discoveredVertices = new ArrayList<>();
+
+		vertexQueue.add(a);
+		while (!vertexQueue.contains(b)) {
+			// Vertex w = vertexQueue.poll();
+			// branch.add(w);
+
+			for (Vertex z : vertexQueue) {
+				if (!discoveredVertices.contains(z)) {
+					discoveredVertices.add(z);
+					downstreamQueue.addAll(g.getDownstreamNeighbors(z));
+
+				}
+
+			}
+			vertexQueue.clear();
+			counter++;
+			if (downstreamQueue.contains(b)) {
+				break;
+			}
+
+			else {
+				// Vertex x = downstreamQueue.poll();
+				// branch.add(w);
+
+				for (Vertex y : downstreamQueue) {
+					if (!discoveredVertices.contains(y)) {
+						discoveredVertices.add(y);
+						vertexQueue.addAll(g.getDownstreamNeighbors(y));
+
+					}
+				}
+				downstreamQueue.clear();
+				counter++;
+			}
+
+		}
+		// listSet.add(branch);
+
+		return counter;
+
+	}
+
 }
